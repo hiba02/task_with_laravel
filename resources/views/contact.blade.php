f9ad5f1b2de79de3ae2c423f71a28aed6d2decf8
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-05-27 21:38:48 -0400

    Remove redundant codes by using a layout.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
+@section('title')
+    Contact
+@endsection
 
-    </head>
-    <body>
-      <h2>Contact</h2>
-    </body>
-</html>
+@section('content')
+    Contact
+@endsection
